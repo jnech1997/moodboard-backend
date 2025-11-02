b53f0078f16f26a9329cd7eb3cb4022bbdffcf39
Joseph Nechleba <jnech16@gmail.com>
2025-11-02 12:24:28 -0500

    reconfigure fly toml to set proxy correctly

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import os
 import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
@@ -67,7 +66,6 @@ if ENV == "production":
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    app.add_middleware(HTTPSRedirectMiddleware)
 
 # Static files
 app.mount("/static", StaticFiles(directory="uploads"), name="static")
